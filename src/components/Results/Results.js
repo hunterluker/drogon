078cfcb7ec6a536c4da6541c4574faf10090fb23
hunterluker <hunterluker1992@gmail.com>
2019-05-21 09:26:09 -0600

    results component title format

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import './Results.css';
 
 export default class Results extends Component {
+  handleSearchFormat = () => {
+    const { search } = this.props;
+    if (search === 'reverseip') {
+      return (
+        search.charAt(0).toUpperCase() +
+        search.slice(1, 7) +
+        '-' +
+        search.charAt(7).toUpperCase() +
+        search.charAt(8).toUpperCase()
+      );
+    } else {
+      return search.charAt(0).toUpperCase() + search.slice(1);
+    }
+  };
   render() {
-    const { data, search } = this.props;
+    const { data } = this.props;
+
     return (
       <div className="results-section">
         <p className="search-type">
-          <span>
-            {search.charAt(0).toUpperCase() + search.slice(1)} Results:
-          </span>
+          <span>{this.handleSearchFormat()} Results:</span>
         </p>
         <div className="data">{data}</div>
         <button>Save</button>
