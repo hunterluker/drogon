08ad602803b995dabd84e4e9c19edd5177147714
hunterluker <hunterluker1992@gmail.com>
2019-05-21 09:51:38 -0600

    add media queries for search results

--- src/utils/buildPingJSX.js
+++ src/utils/buildPingJSX.js
@@ -1,20 +1,13 @@
 import React from 'react';
+import '../styles/buildPing.css';
 
 export const buildPingJSX = data => {
   let pingData = data.split('\n');
 
   let mappedPing = pingData.map((ping, i) => {
     return (
-      <div className="ping-data" style={{ marginBottom: '3px' }} key={i}>
-        <p
-          style={{
-            fontSize: '11px',
-            lineHeight: 1.5,
-            fontFamily: 'monospace'
-          }}
-        >
-          {ping}
-        </p>
+      <div className="ping-data" key={i}>
+        <p>{ping}</p>
       </div>
     );
   });
